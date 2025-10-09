f1ac5e14940b0e0e61009a566418b4310b9e5a0b
elijah <elijahmurket@gmail.com>
2025-10-09 22:57:53 +0300

    featured fashion showing up

---
+++ theme_fashion/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Fashion Theme",
+    "description": "Fashion website theme",
+    "category": "Theme",
+    "sequence": 1000,
+    "version": "1.0",
+    "depends": ["website"],
+    "data": [
+        "views/snippets/feature_fashion.xml",
+        "views/snippets/snippets.xml",
+    ],
+    "images": [],
+    "license": "LGPL-3",
+}
