2fd4c2bb0d95a4a50e74381ebcefc4a47cc7694a
elijah <elijahmurket@gmail.com>
2025-10-10 09:47:24 +0300

    categories and trusted working

--- theme_fashion/__manifest__.py
+++ theme_fashion/__manifest__.py
@@ -9,12 +9,15 @@
     "version": "1.0",
     "depends": ["website"],
     "data": [
+        "views/snippets/trusted_brands_initializer.xml",
         "views/snippets/feature_fashion.xml",
         "views/snippets/snippets.xml",
     ],
     "assets": {
         "web.assets_frontend": [
             "theme_fashion/static/src/js/featured_fashion.js",
+            "theme_fashion/static/src/js/latest_products_carousel.js",
+            "theme_fashion/static/src/js/categories_snippet.js",
         ],
     },
     "images": [],
